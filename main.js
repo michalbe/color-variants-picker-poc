88012d3aff8a66e4edfac43c17afb26413c77416
Michał Budzyński <michal@virtualdesign.pl>
2015-07-09 01:01:00 +0100

    :waxing_crescent_moon: display list of colorsa

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 define(['knockout'], function(ko) {
   ko.components.register('color', { require: 'components/color/color' });
+  ko.components.register('colors', { require: 'components/color-list/color-list' });
   ko.applyBindings({});
 });
