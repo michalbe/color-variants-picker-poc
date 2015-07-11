911613b2aa60030e17a3ac57cc108f6d4ff78216
Michał Budzyński <michal@virtualdesign.pl>
2015-07-11 11:31:25 +0200

    :dromedary_camel: cart component css

---
+++ components/cart/cart.js
@@ -0,0 +1,26 @@
+define(['knockout', 'text!./color.html'], function(ko, template) {
+
+  function ColorViewModel(params) {
+    var qty = ko.observable(0);
+
+    var color = params.color;
+    
+    var increment = function(){
+      var tmp = qty();
+      qty(++tmp);
+    };
+
+    var decrement = function(){
+      var tmp = qty();
+      qty(--tmp);
+    };
+
+    return {
+      qty: qty,
+      color: color,
+      increment: increment,
+      decrement: decrement
+    };
+  }
+  return { viewModel: ColorViewModel, template: template };
+});
